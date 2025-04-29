24943bbd32dafd3afd43c5fb05ecf46014f0397f
jeru-6969 <jeursi957@gmail.com>
2025-04-29 11:00:11 -0300

    cambiamos la pagina para bien, pero hay un error en el navbar que muestra el contenido abajo(esta mal)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { ArmoniaComponent } from './musikita/armonia/armonia.component';
 import { NotasEscalasComponent } from './musikita/notas-escalas/notas-escalas.component';
 import { RitmoCompasComponent } from './musikita/ritmo-compas/ritmo-compas.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 export const routes: Routes = [
-    {path: '',redirectTo:'/armonia',pathMatch:'full'},
+    {path: '',redirectTo:'/inicio',pathMatch:'full'},
     {path:'armonia',component:ArmoniaComponent},
     {path:'notas',component:NotasEscalasComponent},
     {path:'ritmo',component:RitmoCompasComponent},
+    {path:'inicio',component:InicioComponent},
 
 
 ];
---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-inicio',
+  imports: [RouterModule,RouterLink,],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export class InicioComponent {
+
+}
