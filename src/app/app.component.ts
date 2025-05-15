65d0ca266fd4b87b136ca9029ac394e3fb455dfb
jeru-6969 <jeursi957@gmail.com>
2025-05-15 11:05:45 -0300

    todo bien, falta lo de,los colores del nabvar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet,NavigationEnd,Router } from '@angular/router';
 import { NavbarComponent } from './Shared/navbar/navbar.component';
 import { FooterComponent } from './Shared/footer/footer.component';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,FooterComponent,RouterModule],
+  imports: [RouterOutlet,NavbarComponent,NavigationEnd,FooterComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto-recopao';
-}
+  currentRoute: string = '';
+   constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    })
+  }}
